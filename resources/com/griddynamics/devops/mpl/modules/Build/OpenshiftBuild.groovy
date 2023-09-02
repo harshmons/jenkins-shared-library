38f057c27bd48d71c9742d2bb3a77998c4a7b3e5
harshmons <harshverma04111989@gmail.com>
2023-09-02 12:56:21 +0530

    Changed

--- resources/com/griddynamics/devops/mpl/modules/Build/OpenshiftBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/OpenshiftBuild.groovy
@@ -2,6 +2,8 @@
  * Build openshift containers
  */
 
-withCredentials([string(credentialsId: CFG.'openshift.credential', variable: 'OPENSHIFT_TOKEN')]) {
-  echo 'TODO: Build openshift container'
-}
+// withCredentials([string(credentialsId: CFG.'openshift.credential', variable: 'OPENSHIFT_TOKEN')]) {
+//   echo 'TODO: Build openshift container'
+// }
+
+echo "I am from Open shift Build"
--- vars/MPLPipeline.groovy
+++ vars/MPLPipeline.groovy
@@ -52,24 +52,24 @@ def call(body) {
           MPLModule()
         }
       }
-      // stage( 'Build' ) {
-      //   when { expression { MPLModuleEnabled() } }
-      //   steps {
-      //     MPLModule()
-      //   }
-      // }
-      // stage( 'Deploy' ) {
-      //   when { expression { MPLModuleEnabled() } }
-      //   steps {
-      //     MPLModule()
-      //   }
-      // }
-      // stage( 'Test' ) {
-      //   when { expression { MPLModuleEnabled() } }
-      //   steps {
-      //     MPLModule()
-      //   }
-      // }
+      stage( 'Build' ) {
+        when { expression { MPLModuleEnabled() } }
+        steps {
+          MPLModule()
+        }
+      }
+      stage( 'Deploy' ) {
+        when { expression { MPLModuleEnabled() } }
+        steps {
+          MPLModule()
+        }
+      }
+      stage( 'Test' ) {
+        when { expression { MPLModuleEnabled() } }
+        steps {
+          MPLModule()
+        }
+      }
     }
     post {
       always {
